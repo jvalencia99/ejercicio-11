e28ee05cd24078048ceeb0bc0053475864ab5fe6
jvalencia99 <127439985+jvalencia99@users.noreply.github.com>
2023-03-13 14:52:44 -0500

    Add files via upload

---
+++ manage-store-main/manage_store-main/src/main/java/co/edu/umanizales/manage_store/controller/SaleController.java
@@ -0,0 +1,96 @@
+package co.edu.umanizales.manage_store.controller;
+
+import co.edu.umanizales.manage_store.controller.dto.ResponseDTO;
+import co.edu.umanizales.manage_store.controller.dto.SaleDTO;
+import co.edu.umanizales.manage_store.model.Sale;
+import co.edu.umanizales.manage_store.model.Seller;
+import co.edu.umanizales.manage_store.model.Store;
+import co.edu.umanizales.manage_store.service.SaleService;
+import co.edu.umanizales.manage_store.service.SellerService;
+import co.edu.umanizales.manage_store.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "sale")
+public class SaleController {
+    @Autowired
+    private SaleService saleService;
+    @Autowired
+    private SellerService sellerService;
+    @Autowired
+    private StoreService storeService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO> getSales(){
+        return new ResponseEntity<>(
+                new ResponseDTO(200,
+                        saleService.getSales(),
+                null),
+                HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/total")
+    public ResponseEntity<ResponseDTO> getTotalSales(){
+        return new ResponseEntity<>(new ResponseDTO(200, 
+        saleService.getTotalSales(), null),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/total/{code}")
+    public ResponseEntity<ResponseDTO> getTotalSalesBySeller(
+        @PathVariable String code
+    ){
+        return new ResponseEntity<>(new ResponseDTO(200, 
+        saleService.getTotalSalesBySeller(code), null),
+        HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ResponseDTO> createSale(@RequestBody
+                                                  SaleDTO saleDTO){
+        Seller findSeller = sellerService.getSellerById(saleDTO.getSellerId());
+        if( findSeller == null){
+            return new ResponseEntity<>(new ResponseDTO(409,
+                    "El vendedor ingresado no existe",null),
+                    HttpStatus.BAD_REQUEST);
+        }
+        Store findStore = storeService.getStoreById(saleDTO.getStoreId());
+        if( findStore == null){
+            return new ResponseEntity<>(new ResponseDTO(409,
+                    "La tienda ingresada no existe",null),
+                    HttpStatus.BAD_REQUEST);
+        }
+        saleService.addSale(new Sale(findStore,findSeller,
+                saleDTO.getQuantity()));
+        return new ResponseEntity<>(new ResponseDTO(200,
+                "Venta adicionada",null),
+                HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/bestseller")
+    public ResponseEntity<ResponseDTO> getBestSeller(){
+        return new ResponseEntity<>(new ResponseDTO(200,
+                saleService.getBestSeller(sellerService.getSellers()), null),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/cantidadmayorSeller")
+    public ResponseEntity<ResponseDTO> getStoreByQuantity(int cant,String code) {
+        int findSale = saleService.getTotalSalesBySeller(code);
+        while (findSale != 0) {
+            return new ResponseEntity<>(new ResponseDTO(200,saleService.getTotalSalesBySeller(code)>cant,
+                    null),HttpStatus.OK);
+
+        }
+        return new ResponseEntity<>(new ResponseDTO(409, "no hay vendedores con esas ventas mayores ", null),
+                HttpStatus.BAD_REQUEST);
+    }
+
+}
